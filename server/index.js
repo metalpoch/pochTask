853103b65f6863ceeef682a85a123a1ae64ae011
Keiber Urbila <keiberup.dev@gmail.com>
2023-02-06 19:05:02 -0400

    feat(server): validation of tokens on some routes

--- server/index.js
+++ server/index.js
@@ -5,12 +5,16 @@ import MongoStore from "connect-mongo";
 import cors from "cors";
 import morgan from "morgan";
 import { config } from "dotenv";
+import auth from "./routes/auth.js";
 import users from "./routes/users.js";
 import tasks from "./routes/tasks.js";
+import validateToken from "./utils/validateToken.js"
 
 config();
 // Variables
-const { NODE_ENV, PORT, HOST, SECRET } = process.env;
+const { NODE_ENV, PORT, HOST } = process.env;
+export const { SECRET, SECRET_SALT } = process.env;
+
 const DB = NODE_ENV === "development"
   ? process.env.DB_URI_DEV
   : process.env.DB_URI;
@@ -26,15 +30,16 @@ app.use(session({
   saveUninitialized: true,
   store: MongoStore.create({mongoUrl: DB})
 }))
-app.use("/api/users", users);
-app.use("/api/tasks", tasks);
+app.use("/api/auth", auth);
+app.use("/api/users", validateToken, users);
+app.use("/api/tasks", validateToken, tasks);
 
 // Connection with Mongo Atlas
 mongoose.set("strictQuery", false);
 mongoose
   .connect(DB)
   .then(() => console.log("Connection with the database established"))
-  .catch((error) => console.log({ error }));
+  .catch((error) => console.error({ error }));
 
 app.listen(PORT, () => {
   console.log(`Started server on ${HOST}:${PORT}`);
