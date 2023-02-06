2a7aa3a5d5b6c3a2c8420e7af2c7eb464bdd4bb5
Keiber Urbila <keiberup.dev@gmail.com>
2023-02-06 19:25:40 -0400

    refactor(sever): now import SECRET from index.js

--- server/utils/validateToken.js
+++ server/utils/validateToken.js
@@ -1,13 +1,10 @@
-import { config } from "dotenv";
 import jwt from "jsonwebtoken";
-
-config()
-const secret = process.env.SECRET
+import { SECRET } from "../index.js";
 
 const validateToken = (req, res, next) => {
   const token = req.header("auth-token");
   if (!token) return res.status(401).json({ error: "Access Denied" });
-  jwt.verify(token, secret, (err, decoded) => {
+  jwt.verify(token, SECRET, (err, decoded) => {
     if (!err) {
       req.user = decoded;
       next();
