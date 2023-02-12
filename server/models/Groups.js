83f94f28814fc090172bf102116a3b7e4601cb58
Keiber Urbila <keiberup.dev@gmail.com>
2023-02-12 19:21:05 -0400

    feat(server): the Groups model was created

---
+++ server/models/Groups.js
@@ -0,0 +1,32 @@
+import { Schema, model } from "mongoose";
+
+const MemberSchema = new Schema(
+  {
+    rank: { type: String, require: true },
+    score: { type: Number, default: 0 },
+    tasksCompleted: { type: Number, default: 0 },
+    tasksFailed: { type: Number, default: 0 },
+    muted: { type: Boolean, default: false },
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true, versionKey: false }
+);
+
+
+const GroupSchema = new Schema(
+  {
+    name: { type: String, require: true },
+    description: { type: String, require: true },
+    members: { type: [MemberSchema] },
+    leader: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true, versionKey: false }
+);
+
+export default model("Groups", GroupSchema);
