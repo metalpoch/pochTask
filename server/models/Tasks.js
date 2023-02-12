62177b1ee64d4963bd409509d5d98af83a7248bf
Keiber Urbila <keiberup.dev@gmail.com>
2023-02-12 19:09:04 -0400

    feat(server): added new fields to models New fields were added to Tasks and Users models

--- server/models/Tasks.js
+++ server/models/Tasks.js
@@ -1,15 +1,23 @@
 import { Schema, model } from "mongoose";
 
+const AccomplishedSchema = new Schema(
+  {
+    success: { type: Boolean, default: false },
+    comment: { type: String, required: true },
+  },
+  { timestamps: true, versionKey: false }
+);
+
 const TaskSchema = new Schema(
   {
-    title: { type: String, require: true },
-    comment: { type: String, require: true },
-    important: { type: Boolean, require: true },
-    accomplished: { type: Boolean, require: true },
-    user: {
-      type: Schema.Types.ObjectId,
-      ref: "User",
-    },
+    originator: { type: Schema.Types.ObjectId, ref: "User" },
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    comment: { type: String },
+    important: { type: Boolean, default: false },
+    limitDate: { type: Date },
+    liable: { type: Schema.Types.ObjectId, ref: "User"},
+    accomplished: { type: [AccomplishedSchema] }
   },
   { timestamps: true, versionKey: false }
 );
--- server/models/Users.js
+++ server/models/Users.js
@@ -7,10 +7,17 @@ const UserSchema = new Schema(
     alias: { type: String, required: true, unique: true },
     username: { type: String, required: true, unique: true },
     rank: { type: String, required: true },
+    admin: { type: String, default: false },
     password: { type: String, required: true },
     hidden: { type: String, required: true },
     blocked: { type: String, default: false },
     avatar: { type: String, required: true }, // default https://robohash.org/<username>
+    groups: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Groups",
+      },
+    ],
     tasks: [
       {
         type: Schema.Types.ObjectId,
