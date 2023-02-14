febfa4cf75d62c0f97b2d2aa711295b40ab86ca7
Keiber Urbila <keiberup.dev@gmail.com>
2023-02-13 23:51:24 -0400

    feat(server): filter profile data

--- server/routes/profile.js
+++ server/routes/profile.js
@@ -3,29 +3,43 @@ import Users from "../models/Users.js";
 
 const route = Router();
 
-const getProfile = async ({ id, username, details }) => {
+const getProfile = async ({ id, username, details, hidden }) => {
   id ??= false;
   details ??= false;
   return details && id
-    ? await Users.findById(id).populate("Tasks Groups")
-    : await Users.findOne({ username });
+    ? await Users.findById(id, hidden).populate("Tasks Groups")
+    : await Users.findOne({ username }, hidden);
 };
 
 route.get("/", (req, res) => {
   const { id } = req.user;
-  const result = getProfile({id, details: true});
+  const hidden = {
+    _id: 0,
+    password: 0,
+    admin: 0,
+    hidden: 0,
+    username: 0,
+  };
+  const result = getProfile({id, hidden, details: true});
   result
     .then((data) => res.json(data))
     .catch((error) => res.status(400).json({ error: error.message }));
 });
 
-route.get("/:username", (req, res) => {
-  const username = req.params.username;
-  const result = getProfile({username});
+route.get("/:alias", (req, res) => {
+  const username = req.params.alias.toLowerCase();
+  const hidden = {
+    _id: 0,
+    password: 0,
+    tasks: 0,
+    admin: 0,
+    hidden: 0,
+    username: 0,
+  };
+  const result = getProfile({username, hidden});
   result
     .then((data) => res.json(data))
     .catch((error) => res.status(400).json({ error: error.message }));
 });
-
 
 export default route;
